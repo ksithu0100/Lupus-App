48b66191841beca9487d218ea864232d85b83774
ksithu0100 <sithuk@vcu.edu>
2025-10-27 00:01:28 -0400

    Lupus App

--- app/src/main/java/com/example/lupusapp/MainActivity.java
+++ app/src/main/java/com/example/lupusapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lupusapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick  (View view){
+                Intent intent = new Intent (MainActivity.this, loading_page.class);
+                startActivity(intent);
+            }
+
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/lupusapp/loading_page.java
@@ -0,0 +1,11 @@
+package com.example.lupusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class loading_page extends Activity {
+    @ Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_page);
+}}
